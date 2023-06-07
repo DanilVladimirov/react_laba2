e06fe9110fbbf088caf6be97014a420dab48aa8a
Daniil Vladimirov <daniil.vladymyrov@eosda.com>
2023-06-07 12:36:02 +0300

    add login form

--- src/Header.js
+++ src/Header.js
@@ -1,37 +1,40 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import {
     Navbar,
     Nav,
     FormControl,
     Container,
     Form,
-    Button
+    Button,
+    Modal
 } from "react-bootstrap";
 import Contacts from './Pages/Contacts';
 import Home from './Pages/Home';
 import Blog from './Pages/Blog';
 import About from './Pages/About';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from "./logo.png";
 
-export default class Header extends Component {
-    
-    render() {
-        return (
-            <>
+
+export default function Header() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <>
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="/">
-                       <img
-                       src={logo}
-                       height="30"
-                       width="30"
-                       className="d-inline-block align-top"
-                       alt = "Logo"
-                       /> Mc Donalds
+                        <img
+                            src={logo}
+                            height="30"
+                            width="30"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                        /> Mc Donalds
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav claccName="me-auto">
                             <Nav.Link href="/"> Home </Nav.Link>
@@ -41,25 +44,56 @@ export default class Header extends Component {
                         </Nav>
                         <Form className="d-flex">
                             <FormControl
-                                type ="text"
+                                type="text"
                                 placeholder="Search"
                                 className="me-sm-3"
                             />
                             <Button variant="outline-info">Search</Button>
                         </Form>
+                        <Button className="ms-2" onClick={handleShow}>Login</Button>
                     </Navbar.Collapse>
                 </Container>
-                
+
             </Navbar>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log in</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="fromBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter password">
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
             <BrowserRouter >
                 <Routes>
-                    <Route path="/" element={<Home  />}/>
-                    <Route path="/about" element={<About/>}/>
-                    <Route path="/contacts" element={<Contacts/>}/>
-                    <Route path="/blog" element={<Blog/>}/>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                    <Route path="/blog" element={<Blog />} />
                 </Routes>
             </BrowserRouter >
-            </>
-        )
-    }
+        </>
+    )
 }
