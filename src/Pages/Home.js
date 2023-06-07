e92c687ca270a2db597f9faf782d94330800bc8b
Daniil Vladimirov <daniil.vladymyrov@eosda.com>
2023-06-07 11:42:20 +0300

    add teams block on home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CarouselBoxHk from "../CarouselBoxHk";
+import {Button, Card, CardImg, Container} from "react-bootstrap";
 
 class Home extends Component {
     render() {
         return (
-            <CarouselBoxHk/>
+            <>
+                <CarouselBoxHk />
+                <Container>
+                    <h2 className="text-center m-4">Our team</h2>
+                    <div className="row">
+                        <div className="col">
+                            <Card className="m-4 text-center" bg="light" border="primary">
+                                <Card.Img
+                                    variant="top"
+                                    src="https://cdn.geekwire.com/wp-content/uploads/2015/09/FIFA-16-Brazil-Team-600x300.jpg"
+                                />
+                                <Card.Body>
+                                    <Card.Title>Прогери</Card.Title>
+                                    <Card.Text>
+                                        Команда 1
+                                    </Card.Text>
+                                    <Button variant="primary">Про команду</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card className="m-4 text-center" bg="light">
+                                <Card.Img
+                                    variant="top"
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAHOG3xhVMR3mi6xZKkVIm9p53Ad6MxoVhMw&usqp=CAU"
+                                />
+                                <Card.Body>
+                                    <Card.Title>Природознавці</Card.Title>
+                                    <Card.Text>
+                                        Команда 2
+                                    </Card.Text>
+                                    <Button variant="primary">Про команду</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+
+                        <div className="col">
+                            <Card className="m-4 text-center" bg="light">
+                                <Card.Img
+                                    variant="top"
+                                    className=''
+                                    src="https://blog.qbsmsp.com/hs-fs/hubfs/lwteamsstrategies600x300gray.png?width=600&name=lwteamsstrategies600x300gray.png"
+                                />
+                                <Card.Body>
+                                    <Card.Title>Девопсери</Card.Title>
+                                    <Card.Text>
+                                        Команда 3
+                                    </Card.Text>
+                                    <Button variant="primary">Про команду</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                </Container>
+
+            </>
+
         );
     }
 }
